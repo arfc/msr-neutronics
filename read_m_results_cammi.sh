84c788e6c380b8640e0d11133702ff589c0d1665
Alex Lindsay <adlinds3@ncsu.edu>
2016-12-05 17:52:07 -0600

    Reorganize directories.

--- read_m_results_cammi.sh
+++ read_m_results_cammi.sh
@@ -5,21 +5,13 @@ num_precursor_groups=8
 start_column=9
 stop_column=$(echo "$(($start_column + ($num_groups - 1) * 2))")
 precursor_stop_column=$(echo "$(($start_column + ($num_precursor_groups - 1) * 2))")
-# echo $start_column
-# echo $stop_column
-# echo $precursor_stop_column
-# echo $num_precursor_groups
 gsxs_start_column=7
 gsxs_stop_column=$(echo "$(($gsxs_start_column + ($num_groups*$num_groups - 1) * 2))")
 chi_start_column=7
 chi_stop_column=$(echo "$(($chi_start_column + ($num_groups - 1) * 2))")
-root=msr2g_enrU_two_mat_homogenization
-# > $fuel_out
-# > $mod_out
+root=cammi_2g_Th_U_two_mat_homogenization_dens_func
 group_names=""
 for i in $(seq $num_groups); do group_names=$group_names"Group"$i" "; done
-# echo "var_type temp $group_names" >> $fuel_out
-# echo "var_type temp $group_names" >> $mod_out
 
 # fuel_temp_start=750
 # fuel_temp_stop=1000
@@ -31,7 +23,7 @@ for i in $(seq $num_groups); do group_names=$group_names"Group"$i" "; done
 # mod_temp_range=($(seq $mod_temp_start $mod_step $mod_temp_stop))
 
 temp_start=800
-temp_stop=1550
+temp_stop=1500
 temp_step=50
 temp_range=($(seq $temp_start $temp_step $temp_stop))
 
@@ -60,9 +52,22 @@ for index in $(seq $((${#lengths[@]} - 1))); do
     fi
 done
 
-interp_dir=interpolation_files_lock_step
+interp_dir=cammi_geometry
 rm -rf ${interp_dir}
 mkdir ${interp_dir}
+
+fuel_out=${root}_fuel_data_func_of_fuel_temp
+mod_out=${root}_mod_data_func_of_mod_temp
+for temp in ${temp_range[@]}; do
+    echo $temp
+    res_file=${root}_fuel_${temp}_mod_${temp}_res.m
+    for index in "${!xsecs[@]}"; do
+        awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v temp=$temp -v xsec=${xsecs[index]} \
+            'BEGIN {ORS = ""} $1==xsec {j++} j==1 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${fuel_out}_${xsecs[index]}.txt"
+        awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v temp=$temp -v xsec=${xsecs[index]} \
+            'BEGIN {ORS = ""} $1==xsec {j++} j==2 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${mod_out}_${xsecs[index]}.txt"
+    done
+done
 
 # for fuel_temp in ${fuel_temp_range[@]}; do
 #     echo $fuel_temp
@@ -80,31 +85,3 @@ mkdir ${interp_dir}
 #         done
 #     done
 # done
-
-fuel_out=${root}_fuel_interp
-mod_out=${root}_mod_interp
-# > "interpolation_files_lock_step/${fuel_out}_${xsecs[index]}.txt"
-# > "interpolation_files_lock_step/${mod_out}_${xsecs[index]}.txt"
-for temp in ${temp_range[@]}; do
-    echo $temp
-    res_file=${root}_fuel_${temp}_mod_${temp}_res.m
-    for index in "${!xsecs[@]}"; do
-        awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v temp=$temp -v xsec=${xsecs[index]} \
-            'BEGIN {ORS = ""} $1==xsec {j++} j==1 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${fuel_out}_${xsecs[index]}.txt"
-        awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v temp=$temp -v xsec=${xsecs[index]} \
-            'BEGIN {ORS = ""} $1==xsec {j++} j==2 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${mod_out}_${xsecs[index]}.txt"
-    done
-done
-
-for index in "${!xsecs[@]}"; do
-    echo ${start_cols[index]}
-    echo ${stop_cols[index]}
-done
-
-
-
-# column -t $fuel_out > tmp
-# cat tmp > $fuel_out
-# column -t $mod_out > tmp
-# cat tmp > $mod_out
-# rm tmp
--- step_temp_cammi.sh
+++ step_temp_cammi.sh
@@ -4,22 +4,68 @@ function fdens () {
     echo 2.41303 - 0.0004884 \* $1 | bc -l
     }
 
-for temp in {800..1550..50} ; do
-    file=msr2g_enrU_two_mat_homogenization_fuel_${temp}_mod_${temp}
-    cp msr2g_enrU $file
-    sed -i -e "s/\(mat fuel *-\)[0-9.]* tmp [0-9]*\(.*\)/\1$(fdens $temp) tmp $temp \2/" $file
-    sed -i -e "s/\(mat moder.*tmp \)953/\1$temp/" $file
-    if [ $(expr $temp / 900) -eq 0 ]; then
-        sed -i -e "s/09c\(.*% fuel\)/06c\1/" $file
-        sed -i -e "s/09c\(.*% moder\)/06c\1/" $file
-    elif [ $(expr $temp / 1200) -eq 0 ]; then
-	:
-    elif [ $(expr $temp / 1500) -eq 0 ]; then
-	sed -i -e "s/09c\(.*% fuel\)/12c\1/" $file
-        sed -i -e "s/09c\(.*% moder\)/12c\1/" $file
-    else
-	sed -i -e "s/09c\(.*% fuel\)/15c\1/" $file
-        sed -i -e "s/09c\(.*% moder\)/15c\1/" $file
-    fi
-    sss -mpi 4 $file
+for fuel_temp in {800..1500..50} ; do
+    for mod_temp in {1550..2050..50} ; do
+	file=msr2g_Th_U_two_mat_homogenization_fuel_${fuel_temp}_mod_${mod_temp}
+	cp msr2g_Th_U $file
+	sed -i -e "s/\(mat fuel.*tmp \)922/\1 $fuel_temp/" $file
+	sed -i -e "s/\(mat moder.*tmp \)953/\1 $mod_temp/" $file
+	if [ $(expr $fuel_temp / 900) -eq 0 ]; then
+            sed -i -e "s/09c\(.*% fuel\)/06c\1/" $file
+	elif [ $(expr $fuel_temp / 1200) -eq 0 ]; then
+	    :
+	elif [ $(expr $fuel_temp / 1500) -eq 0 ]; then
+	    sed -i -e "s/09c\(.*% fuel\)/12c\1/" $file
+	elif [ $(expr $fuel_temp / 1800) -eq 0 ]; then
+	    sed -i -e "s/09c\(.*% fuel\)/15c\1/" $file
+	else
+	    sed -i -e "s/09c\(.*% fuel\)/18c\1/" $file
+	fi
+
+	if [ $(expr $mod_temp / 900) -eq 0 ]; then
+            sed -i -e "s/09c\(.*% moder\)/06c\1/" $file
+	elif [ $(expr $mod_temp / 1200) -eq 0 ]; then
+	    :
+	elif [ $(expr $mod_temp / 1500) -eq 0 ]; then
+            sed -i -e "s/09c\(.*% moder\)/12c\1/" $file
+	elif [ $(expr $mod_temp / 1800) -eq 0 ]; then
+            sed -i -e "s/09c\(.*% moder\)/15c\1/" $file
+	else
+            sed -i -e "s/09c\(.*% moder\)/18c\1/" $file
+	fi
+	sss -mpi 3 $file
+    done
+done
+
+for fuel_temp in {1550..2050..50} ; do
+    for mod_temp in {800..1500..50} ; do
+	file=msr2g_Th_U_two_mat_homogenization_fuel_${fuel_temp}_mod_${mod_temp}
+	cp msr2g_Th_U $file
+	sed -i -e "s/\(mat fuel.*tmp \)922/\1 $fuel_temp/" $file
+	sed -i -e "s/\(mat moder.*tmp \)953/\1 $mod_temp/" $file
+	if [ $(expr $fuel_temp / 900) -eq 0 ]; then
+            sed -i -e "s/09c\(.*% fuel\)/06c\1/" $file
+	elif [ $(expr $fuel_temp / 1200) -eq 0 ]; then
+	    :
+	elif [ $(expr $fuel_temp / 1500) -eq 0 ]; then
+	    sed -i -e "s/09c\(.*% fuel\)/12c\1/" $file
+	elif [ $(expr $fuel_temp / 1800) -eq 0 ]; then
+	    sed -i -e "s/09c\(.*% fuel\)/15c\1/" $file
+	else
+	    sed -i -e "s/09c\(.*% fuel\)/18c\1/" $file
+	fi
+
+	if [ $(expr $mod_temp / 900) -eq 0 ]; then
+            sed -i -e "s/09c\(.*% moder\)/06c\1/" $file
+	elif [ $(expr $mod_temp / 1200) -eq 0 ]; then
+	    :
+	elif [ $(expr $mod_temp / 1500) -eq 0 ]; then
+            sed -i -e "s/09c\(.*% moder\)/12c\1/" $file
+	elif [ $(expr $mod_temp / 1800) -eq 0 ]; then
+            sed -i -e "s/09c\(.*% moder\)/15c\1/" $file
+	else
+            sed -i -e "s/09c\(.*% moder\)/18c\1/" $file
+	fi
+	sss -mpi 3 $file
+    done
 done
