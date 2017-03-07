001972338e1ab3f71909e299d8d49a40294208a3
Alex Lindsay <alexlindsay239@gmail.com>
2017-03-07 14:45:13 -0600

    Add bash script for reading b1 results. Also remove txt file result output. Again results handling is not what git is for.

--- bash_utils/read_b1_results.sh
+++ bash_utils/read_b1_results.sh
@@ -13,20 +13,10 @@ chi_stop_column=$(echo "$(($chi_start_column + ($num_groups - 1) * 2))")
 group_names=""
 for i in $(seq $num_groups); do group_names=$group_names"Group"$i" "; done
 
-## For bivariable interpolation
-# fuel_temp_start=750
-# fuel_temp_stop=1000
-# mod_temp_start=750
-# mod_temp_stop=1000
-# fuel_step=50
-# mod_step=50
-# fuel_temp_range=($(seq $fuel_temp_start $fuel_step $fuel_temp_stop))
-# mod_temp_range=($(seq $mod_temp_start $mod_step $mod_temp_stop))
-
 # For monovariable interpolation
-temp_start=900
-temp_stop=1000
-temp_step=50
+temp_start=902
+temp_stop=942
+temp_step=20
 temp_range=($(seq $temp_start $temp_step $temp_stop))
 
 xsecs=(B1_FLUX B1_REMXS B1_FISSXS NUBAR B1_NSF FISSE B1_DIFFCOEF RECIPVEL)
@@ -55,14 +45,16 @@ for index in $(seq $((${#lengths[@]} - 1))); do
 done
 
 # Create directory where group constant files in moltres compataible form are stored
-interp_dir=moltres
+interp_dir=moltres_b1
 rm -rf ${interp_dir}
 mkdir ${interp_dir}
 
 # root file on which both this script's input and output are based
-root=msr2g_part_U_full_core
-fuel_out=${root}_fuel_data_func_of_fuel_temp
-mod_out=${root}_mod_data_func_of_mod_temp
+# echo "What's root name of file?"
+# read root
+root=msre_homogeneous_critical_question_mark
+# fuel_out=${root}_fuel_data_func_of_fuel_temp
+# mod_out=${root}_mod_data_func_of_mod_temp
 
 # Look over monovariate temperature
 for temp in ${temp_range[@]}; do
@@ -71,26 +63,13 @@ for temp in ${temp_range[@]}; do
     res_file=${root}_${temp}_res.m
     for index in "${!xsecs[@]}"; do
         awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v temp=$temp -v xsec=${xsecs[index]} \
-            'BEGIN {ORS = ""} $1==xsec {j++} j==1 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${fuel_out}_${xsecs[index]}.txt"
-        awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v temp=$temp -v xsec=${xsecs[index]} \
-            'BEGIN {ORS = ""} $1==xsec {j++} j==2 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${mod_out}_${xsecs[index]}.txt"
+            'BEGIN {ORS = ""} $1==xsec {j++} j==1 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${root}_${xsecs[index]}.txt"
+        # awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v temp=$temp -v xsec=${xsecs[index]} \
+        #     'BEGIN {ORS = ""} $1==xsec {j++} j==1 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${fuel_out}_${xsecs[index]}.txt"
+        # awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v temp=$temp -v xsec=${xsecs[index]} \
+        #     'BEGIN {ORS = ""} $1==xsec {j++} j==2 {print temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "${interp_dir}/${mod_out}_${xsecs[index]}.txt"
     done
 done
 
-## Loop over bivariate temperature
-# for fuel_temp in ${fuel_temp_range[@]}; do
-#     echo $fuel_temp
-#     mod_out=${root}_fuel_${fuel_temp}_mod_interp
-#     > "interpolation_files/${mod_out}_${xsecs[index]}.txt"
-#     for index in "${!xsecs[@]}"; do
-#         for mod_temp in ${mod_temp_range[@]}; do
-#             # fuel_out=${root}_mod_${mod_temp}_fuel_interp
-#             # > ${fuel_out}_${xsecs[index]}.txt
-#             res_file=${root}_fuel_${fuel_temp}_mod_${mod_temp}_res.m
-#             # awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v fuel_temp=$fuel_temp -v xsec=${xsecs[index]} \
-#             #     'BEGIN {ORS = ""} $1==xsec {j++} j==1 {print fuel_temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> ${fuel_out}_${xsecs[index]}.txt
-#             awk -v start_column=${start_cols[index]} -v stop_column=${stop_cols[index]} -v mod_temp=$mod_temp -v xsec=${xsecs[index]} \
-#                 'BEGIN {ORS = ""} $1==xsec {j++} j==2 {print mod_temp" "; for (i=start_column; i <= stop_column; i=i+2) print $i" "; print "\n"; exit}' $res_file >> "interpolation_files/${mod_out}_${xsecs[index]}.txt"
-#         done
-#     done
-# done
+cd $interp_dir
+for f in *B1*.txt; do mv "$f" "`echo $f | sed 's/_B1//'`"; done
